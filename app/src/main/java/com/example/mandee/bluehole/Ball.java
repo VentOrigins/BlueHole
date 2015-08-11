415199f0ef2df9a75334ba71e0f38a1ab880f1c3
Adrian Mandee <adrianmandeeeee@gmail.com>
2015-08-10 19:04:46 -0700

    Images for v1 and example for moving with ball class

---
+++ app/src/main/java/com/example/mandee/bluehole/Ball.java
@@ -0,0 +1,18 @@
+package com.example.mandee.bluehole;
+
+import android.widget.ImageView;
+
+/**
+ * Created by MANDEE on August/10/15.
+ */
+public class Ball {
+
+    private float speed;
+    private int number;
+
+
+    public Ball(float speed, int number, ImageView iv) {
+        this.speed = speed;
+        this.number = number;
+    }
+}
--- app/src/main/java/com/example/mandee/bluehole/BlueHoleMain.java
+++ app/src/main/java/com/example/mandee/bluehole/BlueHoleMain.java
@@ -1,16 +1,70 @@
 package com.example.mandee.bluehole;
 
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.util.Log;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
+
 
 public class BlueHoleMain extends ActionBarActivity {
+    Handler h = new Handler();
+    int delay = 1000; //milliseconds
+    Random rand = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blue_hole_main);
+        RelativeLayout rlayout = (RelativeLayout) findViewById(R.id.rlayout);
+        ImageView iv = new ImageView(this);
+        iv.setBackgroundResource(R.drawable.vo);
+        rlayout.addView(iv);
+
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+
+        rlayout.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                ImageView image = (ImageView) findViewById(R.id.bluehole);
+                int x = image.getRight();
+                int y = image.getTop();
+                System.out.println(x);
+                System.out.println(y);
+                image.setX((int) event.getX());
+                image.setY((int) event.getY());
+                return true;
+            }
+
+        });
+
+        h.postDelayed(new Runnable() {
+            public void run() {
+                ImageView image = (ImageView) findViewById(R.id.bluehole);
+                int randomNum = rand.nextInt((300 - 1) + 1) + 1;
+                image.setX(randomNum);
+                image.setY(randomNum);
+
+
+                h.postDelayed(this, delay);
+            }
+        }, delay);
+
+
+
+
+
     }
 
     @Override
@@ -19,6 +73,7 @@ public class BlueHoleMain extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_blue_hole_main, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -34,4 +89,6 @@ public class BlueHoleMain extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
