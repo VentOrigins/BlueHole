e633aa182b19505ff148b6485f740ee82522ebc1
Randy Truong <rtruo001@ucr.edu>
2015-09-09 16:44:33 -0700

    Fixed scaling in terms of moving before game starts and when game overs

--- app/src/main/java/com/example/mandee/bluehole/Game.java
+++ app/src/main/java/com/example/mandee/bluehole/Game.java
@@ -268,6 +268,9 @@ public class Game {
         textBar.setVisibility(View.VISIBLE);
         textBar.setText("Game Over");
         isGameOver = true;
+        // Scales the portal image size back to the original size
+        blueHole.getImage().setScaleX(1.0f);
+        blueHole.getImage().setScaleY(1.0f);
     }
 
     /*  =============================================================================
@@ -331,6 +334,10 @@ public class Game {
 
     public int getBallMovementSpeed() {
         return ballMovementSpeed;
+    }
+
+    public BlueHole getBlueHole() {
+        return blueHole;
     }
 
     private void setBallSpawnSpeed(int spawnSpeed) {
